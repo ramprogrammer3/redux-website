30c8ae9ced3111572e7497a14b361dc34d818dc0
ram kumar <72140644+ramprogrammer3@users.noreply.github.com>
2022-10-05 06:55:40 +0530

    addingbanner

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+
+import "./header.css";
+const Header = () =>{
+    return(
+        <div className="banner-container">
+            <div className="banner">
+                
+            </div>
+        </div>
+    )
+}
+export default Header;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+const Navbar = () =>{
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <Link className="navbar-brand logo" to="/">redux website</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="ms-auto navbar-nav mb-2 mb-lg-0 ">
+                    <li className="nav-item">
+                    <Link className="nav-link " aria-current="page" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/"><i className="fa-solid fa-cart-shopping cart-icon" ></i><span className="cart-span">0</span></Link>
+                        
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
